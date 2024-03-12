cf382a302096739308728cc7221af3b67463c178
Adam Reňak <adam.renak@student.tuke.sk>
2024-03-12 12:00:09 +0100

    vyčistenie kódu, odstránenie zbytočných súborov

--- zobrazenie-mikrofonu.js
+++
@@ -1,7 +0,0 @@
-const Store = require('electron-store');
-const store = new Store();
-
-window.onload = () => {
-  const savedMicrophone = store.get('selectedMicrophone');
-  document.getElementById('saved-microphone').textContent = savedMicrophone || 'Nebol uložený žiadny mikrofón';
-};
